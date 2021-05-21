b41f42d64a8d7a9ad1eff55eea51b48c9de16b73
影山榮一 <a1kageyama@kageyamBookpuro.AirPort>
2021-05-21 15:38:43 +0900

    Update

--- CSharp_LINQ_Projection.cs
+++ CSharp_LINQ_Projection.cs
@@ -4,59 +4,59 @@ using System.Collections.Generic;
 
 namespace Projection
 {
-	class MainClass
-	{
-		public static void Main(string[] args)
-		{
-			var proj = new ProjectionClass();
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var projectionClass = new ProjectionClass();
 
-			proj.GetProje();
+            projectionClass.GetProje();
 
-		}
-	}
+        }
+    }
 
-	class ProjectionClass
-	{
-		public void GetProje()
-		{
-			var source = new[] {
-				new {Name = "Kenji", Age = 38}, 
-				new {Name = "Mayuko", Age = 22}, 
-				new {Name = "Kenichirou", Age = 62}, 
-				new {Name = "Yukio", Age = 22},  
-			};
+    class ProjectionClass
+    {
+        public void GetProje()
+        {
+            var source = new[] {
+                new { Name = "Evans", Age = 38 },
+                new { Name = "Smith", Age = 22 },
+                new { Name = "Jones", Age = 62 },
+                new { Name = "Wilson", Age = 22 },
+            };
 
-			// Select()
-			// 1つの要素を単一の要素に射影
-			var name = source.Select(e => e.Name);
-			foreach (var n in name)
-				Console.WriteLine(n);
-			Console.WriteLine();
+            // Select()
+            // Projection of one element to a single element
+            var name = source.Select(e => e.Name);
+            foreach (var n in name)
+                Console.WriteLine(n);
+            Console.WriteLine();
 
-			// Select()
-			// Select()を使って正方形の面積を算出
-			IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
-			foreach (int num in squares)
-				Console.WriteLine(num);
-			Console.WriteLine();
+            // Select()
+            // Calculate the area of a square using Select()
+            IEnumerable<int> squares = Enumerable.Range(1, 10).Select(x => x * x);
+            foreach (int num in squares)
+                Console.WriteLine(num);
+            Console.WriteLine();
 
-			// SelectMany()
-			// 1つの要素から複数の要素に射影
-			var nameM = source.SelectMany(e => e.Name);
-			foreach (var n in nameM)
-				Console.Write("{0} ", n);
-			Console.WriteLine();
-			Console.WriteLine();
+            // SelectMany()
+            // Projection from one element to multiple elements
+            var nameM = source.SelectMany(e => e.Name);
+            foreach (var n in nameM)
+                Console.Write("{0} ", n);
+            Console.WriteLine();
+            Console.WriteLine();
 
-			// GroupBy()
-			// 指定のキーて要素をグループ化
-			IEnumerable<IGrouping<int, string>> query = source.GroupBy(e => e.Age, n => n.Name);
-			foreach (IGrouping<int, string> friGroup in query)
-			{
-				Console.WriteLine(friGroup.Key);
-				foreach (string nameF in friGroup)
-					Console.WriteLine(" -> {0}", nameF);
-			}
-		}
-	}
+            // GroupBy()
+            // Group elements by a specified key
+            IEnumerable<IGrouping<int, string>> query = source.GroupBy(e => e.Age, n => n.Name);
+            foreach (IGrouping<int, string> friGroup in query)
+            {
+                Console.WriteLine(friGroup.Key);
+                foreach (string nameF in friGroup)
+                    Console.WriteLine(" -> {0}", nameF);
+            }
+        }
+    }
 }
